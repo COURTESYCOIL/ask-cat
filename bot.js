8e951586e97684e5e434a6caac1bef11e814194d
COURTESYCOIL <forbusiness6886543@gmail.com>
2025-11-30 16:17:51 +0200

    Login fix

---
+++ bot.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isCommand()) return;
+
+    const { commandName } = interaction;
+
+    if (commandName === 'progress') {
+        await interaction.reply('Checking your progress...');
+    } else if (commandName === 'login') {
+        await interaction.reply('Please visit our website to log in.');
+    }
+});
+
+client.login(process.env.DISCORD_TOKEN);
